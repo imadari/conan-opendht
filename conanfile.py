2d6309a92d2c1b89b32c2ef65c50f1b251ef7f44
istvan <istvan.madari@vanderbilt.edu>
2018-09-17 13:11:44 -0500

    license/url/description update

--- conanfile.py
+++ conanfile.py
@@ -4,9 +4,9 @@ from conans import ConanFile, CMake, tools
 class OpendhtConan(ConanFile):
     name = "opendht"
     version = "1.7.4"
-    license = "<Put the package license here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Opendht here>"
+    license = "GPLv3"
+    url = "https://github.com/imadari/conan-opendht"
+    description = "A lightweight C++11 Distributed Hash Table implementation."
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=True"
@@ -21,9 +21,6 @@ class OpendhtConan(ConanFile):
 
 
     def build(self):
-        print("qrva anyad")
-        print(self.deps_cpp_info["msgpack"].rootpath)
-        print("az")
         vars = {'PKG_CONFIG_GnuTLS_PREFIX': self.deps_cpp_info["GnuTLS"].rootpath,
                 'PKG_CONFIG_msgpack_PREFIX': self.deps_cpp_info["msgpack"].rootpath,
                 'PKG_CONFIG_PATH': "%s:%s" % (self.deps_cpp_info["GnuTLS"].rootpath,self.deps_cpp_info["msgpack"].rootpath)}
@@ -35,11 +32,7 @@ class OpendhtConan(ConanFile):
 
     def package(self):
         self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["opendht"]
